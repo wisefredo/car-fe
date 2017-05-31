d9c28644ccad6e62be73b9bbe35cb5d61eb7b299
Carlos Henriquez <carloschulo@gmail.com>
2017-05-30 23:17:36 -0400

    nav logic for state  complete

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,7 @@ const busCusBtn = document.querySelector('#bc-btn');
 
 /*Nav Buttons*/
 const navParent = document.querySelector('.nav-parent');
+const sideOptions = document.querySelector('.side-options');
 const homebtn = document.querySelector('#home-btn');
 const browseAisles = document.querySelector('#browse-aisles');
 const deliveryPickup = document.querySelector('#delivery');
@@ -13,21 +14,32 @@ const register = document.querySelector('#register');
 const homeScreen = document.querySelector('#home-screen');
 const custScreen = document.querySelector('#customer-segment');
 
+/*reusable function to control state of nav selection*/
 function setActiveState(el, array) {
-  // if(!e.target.matches('a')) return;
   array.forEach(elem => {
     elem.classList.remove('active');
   })
-  el.parentNode.classList.add('active');
+  el.classList.add('active');
 }
 
+// nav state
 function navState(e) {
   if (!e.target.matches('a')) return;
-  const elem = e.target;
+  const elem = e.target.parentNode;
   const nodeListArr = [...navParent.querySelectorAll('ul li')];
   console.log(nodeListArr)
   setActiveState(elem, nodeListArr);
 }
+// side nav state
+function sideState(e) {
+  if (!e.target.matches('button')) return;
+  const elem = e.target;
+  const nodeListArr = [...sideOptions.querySelectorAll('button')];
+  console.log(nodeListArr)
+  setActiveState(elem, nodeListArr);
+}
+/*event delegation to handle click of nav items*/
+sideOptions.addEventListener('click', sideState);
 navParent.addEventListener('click', navState);
 
 
