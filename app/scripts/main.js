3317741df449aea0db54d56697caa121095f596b
Carlos Henriquez <carloschulo@gmail.com>
2017-05-30 21:26:08 -0400

    added side nav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,17 @@
 const homeCusBtn = document.querySelector('#hc-btn');
 const busCusBtn = document.querySelector('#bc-btn');
+
+/*Nav Buttons*/
 const homebtn = document.querySelector('#home-btn');
+const browseAisles = document.querySelector('#browse-aisles');
+const deliveryPickup = document.querySelector('#delivery');
+const login = document.querySelector('#login');
+const register = document.querySelector('#register');
+
+/*elements to be shown depending on what button is clicked*/
 const homeScreen = document.querySelector('#home-screen');
 const custScreen = document.querySelector('#customer-segment');
+
 
 function customerScreen(evt) {
   homeScreen.style.display = 'none';
@@ -10,7 +19,6 @@ function customerScreen(evt) {
 }
 
 function goHome(evt) {
-
   homeScreen.style.display = 'flex';
   custScreen.style.display = 'none';
 }
